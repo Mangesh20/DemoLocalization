3e88f25431af8840dc4adf77c544193198c0662d
Mangesh Tekale <mangesh20@gmail.com>
2015-01-17 23:49:34 +0530

    Initial Commit

---
+++ DemoLocalization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoLocalization
+//
+//  Created by Mangesh Tekale on 17/01/15.
+//  Copyright (c) 2015 ComedyMob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoLocalization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoLocalization
+//
+//  Created by Mangesh Tekale on 17/01/15.
+//  Copyright (c) 2015 ComedyMob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
